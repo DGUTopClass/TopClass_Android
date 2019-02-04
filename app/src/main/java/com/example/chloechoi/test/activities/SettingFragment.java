def55d0ebd20078ba401495a88cb41ef73d88dc9
sunnyangee <hwagae2003@gmail.com>
2019-02-04 13:11:35 +0900

    nothig....

--- app/src/main/java/com/example/chloechoi/test/activities/SettingFragment.java
+++ app/src/main/java/com/example/chloechoi/test/activities/SettingFragment.java
@@ -1,4 +1,26 @@
 package com.example.chloechoi.test.activities;
 
-public class SettingFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.chloechoi.test.R;
+
+
+
+public class SettingFragment extends Fragment{
+
+    Button backbtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
