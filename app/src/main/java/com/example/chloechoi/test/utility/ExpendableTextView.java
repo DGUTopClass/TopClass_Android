b34d694312aa366db0d1b928ac780323b5953efa
Yang Si Yeon <mn04098@naver.com>
2019-02-24 22:45:49 +0900

    restart!

---
+++ app/src/main/java/com/example/chloechoi/test/utility/ExpendableTextView.java
@@ -0,0 +1,73 @@
+package com.example.chloechoi.test.utility;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.chloechoi.test.R;
+
+public class ExpendableTextView extends android.support.v7.widget.AppCompatTextView implements View.OnClickListener {
+
+    private static final int MAX_LINES = 4;
+    private int currentMaxLines = Integer.MAX_VALUE;
+
+    public ExpendableTextView(Context context) {
+        super(context);
+        setOnClickListener(this);
+    }
+
+    public ExpendableTextView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        setOnClickListener(this);
+    }
+
+    public ExpendableTextView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        setOnClickListener(this);
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter)
+    {
+        /* If text longer than MAX_LINES set DrawableBottom - I'm using '...' icon */
+        post(new Runnable()
+        {
+            public void run()
+            {
+                if (getLineCount()>MAX_LINES)
+                    setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, R.drawable.switch_thumb);
+                else
+                    setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+
+                setMaxLines(MAX_LINES);
+            }
+        });
+    }
+
+
+    @Override
+    public void setMaxLines(int maxLines)
+    {
+        currentMaxLines = maxLines;
+        super.setMaxLines(maxLines);
+    }
+
+    /* Custom method because standard getMaxLines() requires API > 16 */
+    public int getMyMaxLines()
+    {
+        return currentMaxLines;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        /* Toggle between expanded collapsed states */
+        if (getMyMaxLines() == Integer.MAX_VALUE)
+            setMaxLines(MAX_LINES);
+        else
+            setMaxLines(Integer.MAX_VALUE);
+    }
+}
