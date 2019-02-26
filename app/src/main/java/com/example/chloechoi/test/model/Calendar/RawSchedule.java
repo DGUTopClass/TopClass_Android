d1c9d2f7baad1259cff6e129e916b388ed1b91d5
cchloe2311 <cchloe2311@daum.net>
2019-02-26 14:49:35 +0900

    2019/02/26 added Calendar related model classes

---
+++ app/src/main/java/com/example/chloechoi/test/model/Calendar/RawSchedule.java
@@ -0,0 +1,14 @@
+package com.example.chloechoi.test.model.Calendar;
+
+public class RawSchedule {
+    private String id;
+    private String title;
+    private String startDate;
+    private String startTime;
+    private String finishDate;
+    private String finishTime;
+
+    public RawSchedule(){
+
+    }
+}
